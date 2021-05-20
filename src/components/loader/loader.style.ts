5498a6ede9a9f34c48c9c01680e640d8878b74ba
Christopher Cudiamat <christophervcudiamat.13@gmail.com>
2021-05-20 12:55:58 +0800

    AA

--- src/components/loader/loader.style.ts
+++ src/components/loader/loader.style.ts
@@ -1,33 +1,28 @@
-import styled  from "styled-components/macro";
+import styled from "styled-components/macro";
 import { Modal } from "../ui/modal.style";
-
 
 export const LoaderUnderlay = styled(Modal)`
   height: 100vh;
   width: 100vw;
+  color: red;
   opacity: 0.7;
-  background: ${({theme}) => theme.color.white};
-`
+  background: ${({ theme }) => theme.color.white};
+`;
 
 export const LoaderStyled = styled.img`
   width: 9rem;
   height: 9rem;
   position: fixed;
-  z-index: 999999999;  
+  z-index: 999999999;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  @media ${({theme}) => theme.media.tablet} { 
+  @media ${({ theme }) => theme.media.tablet} {
     width: 10rem;
     height: 10rem;
   }
-  @media ${({theme}) => theme.media.laptop} { 
+  @media ${({ theme }) => theme.media.laptop} {
     width: 12rem;
     height: 12rem;
   }
-`
-
-
-
-
-
+`;
